7213b99bf15fda70c29576b9fc5544b8a2eef705
tcl-s <ltc15010080@outlook.com>
2025-10-08 22:53:36 +0800

    Add files via upload

--- FileName.c
+++ FileName.c
@@ -1,24 +1,58 @@
-#include <stdio.h>
-void display(int a ,int b[]) {
-	for (int i = 1; i <= a; i++) {
-		printf("%d\n", b[i-1]);
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void displayArray(int arr[], int a) {
+	for (int i = 0; i < a; i++) {
+		printf("%d:%d\n", i+1,arr[i]);
 	}
 	return;
 }
-int main() {
-	int arr[5] = { 1,2,3,4,5 };
-	int a = arr[0];
-	int b = arr[2];
-	int c = arr[4];
-	/*display(5, arr[5]);*/
-	printf("%d\n", a);
-	printf("%d\n", b);
-	printf("%d\n", c);
-	printf("%d\n", arr[0]);
-	int sigma = a + b + c;
-	printf("%d\n", sigma);
-	arr[4] = 10;
-	printf("%d\n", arr[4]);
-	/*display(5, arr[5]);*/
-	return 0;
+int main(void) {
+	srand(time(NULL));
+	int arr[90];
+	int num;
+	int count = 0;
+	arr[0] = rand() % 100 + 1;
+	for (int i = 1; i <= 89; i++) {
+		count = 0;
+		num = rand() % 100 + 1;
+		for (int j = 0; j <= i-1; j++) {
+			if (num == arr[j]) {
+				i--;
+				count++;
+				break;
+			}
+			else {
+				//pass
+			}
+		}
+		if (count == 0) {
+			arr[i] = num;
+		}
+		else {
+			//pass
+		}
+	}
+	displayArray(arr, 90);
+	int sigma=0;
+	for (int i = 0; i < 90; i++) {
+		sigma += arr[i];
+	}
+	float ave = sigma / 90;
+	int arr_1[60];
+	int j = 0;
+	for (int i = 0; i < 90; i++) {
+		if (arr[i] > ave) {
+			arr_1[j] = arr[i];
+			j++;
+		}
+		else {
+			//pass
+		}
+	}
+	printf("The average number of the array is:%f\n", ave);
+	displayArray(arr_1, j);
+	printf("%d\n", j);
+	return;
 }
